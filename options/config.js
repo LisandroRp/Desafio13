63ac4de3044c0e383e628ef0288a5af97e5ec604
Lisandro Rodriguez Prados <lisandrorp1997@gmail.com>
2022-02-13 21:59:33 -0300

    Desafio 13 completo

---
+++ options/config.js
@@ -0,0 +1,22 @@
+export default {
+    mongodb: {
+        cnxStr: 'mongodb+srv://admin:admin@cluster0.msysu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+        }
+    },
+    firebase: {
+        "type": "service_account",
+        "project_id": "trabajo-final-coderhouse",
+        "private_key_id": "f282640449c082f57493ba3b92e5619589a7455f",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCzjpGrqD7K4pUy\nAW4WlUBIV8cgodYvRgE3WWBuDuI5bfrw2ZnHlmhuXs5dwQJ39A8pzBrwc5XXs0vT\na1XVwP/ty7Wk1/V6KqRqW0QnHsetsfdNVWf5sfjHlqouLp8xE8cZwlOCFnvD1dMg\ngmZ6hjK2UddN3kGrUwqCL2w52jqCFV90/Gxm1ah7ccF4T/n2sDVzB7KPT0m+n2Dh\nVx0+3KSGd0u/6EENjuHL0TO2aOX976H8YDsH7Yopqy6sWZKM/Av3Cr3G1+BuAXAR\nyFIoguZFthez6HJuRx5oDooxCcaJMRJcg+8m0AGoAuLk2QIIlRoueWgW4W3idNyu\nQw/9zwUTAgMBAAECggEALCNJr7al6N2V6zrqwuZhbOcW7fCaY/iDaAWybJuABLoC\nS55R7PwOI8NuWyGM58u4xn+Dx8PB+8n5Q7lI2kidIOlId4MWtrcwv2w77pnkX9A1\niG8MJhcXnohswPl47Gl+MdjRClNYjSVMxtsHMes3IBYyJNQTtHevR508r2PzqfII\nUfz0A56Flw2VOWliJqZOyETG7Zo0kxMiDqdjOV1YAOU24LtO6YmcjAveDsUefABg\nYDIwdUwQl2FbdzX2uhBsQRINErspUtBpR7ozrxPyod09bzm6VHqs6x7/kenJXrd0\nSRXIZN7KAM8XaqccJBIGXKGnR7abH+IK2+I0LhDWsQKBgQD7EWL2cIGe8uIM+Voh\nX9y7X+QIzajKNiT8WUjwRVFOtdO3Pp2t8PVYsIB++Pz1vLuEcFrYN7R2mR5cq0vw\nPsPaAH+sKOxZ+lKTpofUWIh0+0FjmA2HKUqgEsr2QU/zpY/U4Jg76OAlK82MM0Wa\ndL0iRMFfXVTav4jtBgE4TlQ6nwKBgQC3FY497B4uHuano/HA0OpWN1BMDv0RdBQ6\nnXQBbBNm4LucSti9LU75jhp8MMUQ0Bx+6pgD9CjWfUqTAQ/0ffgF9CefSVBd9m+1\nrNNMsI1ffwZGIy2fHiIh4keiipt73Qm4yag8fvFvvDsORIZs1aDyoHZX1so3sXmT\nFDO7A6cVDQKBgQDs6KYaZ2VYqFW36l8rGPr/cGeMCwxX6ymRWWrjFnW3sQmbYyGE\n7deFnd9ScP65nyLOwPpoBoBqXOfyh8z7YS6L+mZ+/IqyCOx82OpY7EWvJRu5r9Qs\nE0GQtMsB/fVEsph7RodAGOnRCXlG72ahbcC+omRZ6YbVd2DsT/s8t2TxfwKBgC4U\nl9OQDVyQXH+PLPbZ3lFK5MPv9dQhwkGqhZsiB5QaNj41PG4I7Fq1YIcng0IjDzkG\ndLJyDqFFsbJkTw67ceSVui9GNZsgvqccU6Z2vW5uuzaF/iKRZAfhOyR2/IFldd/z\nqYtoa/4ygy6pIqhb8IGAz6h2IDl6St4w9IA6gCA1AoGAbpLPmj+o5uzv/TmKYAEq\nUmn9qmr4KpPwRs3K/9f12ohE0Ida2lhEOGuIhvkaziZFjGjjyUrwvvvtAVzQJXUG\nfiI9S+NkyGHmMi1HJIK1Nfev8YF2w89mw4E0Eg+YkDZxHO6Nk07tjc5j3eLKbJj8\nKq+PSgupcFhjacJrVJQoJ+M=\n-----END PRIVATE KEY-----\n",
+        "client_email": "firebase-adminsdk-sb26z@trabajo-final-coderhouse.iam.gserviceaccount.com",
+        "client_id": "103253333739183409502",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-sb26z%40trabajo-final-coderhouse.iam.gserviceaccount.com"
+    },
+}
